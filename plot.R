017e251903516fd60042625c4dad339fe8b6cdde
masger <michaelasger@gmail.com>
2017-06-10 10:06:42 +0200

    fixed - the second plot

--- plot.R
+++ plot.R
@@ -7,8 +7,10 @@ lymf %>%
   ggplot(aes(I(diffdage/365), loglymf, group = ProjectID)) +
   geom_line(alpha = 1/5) + theme_minimal() + xlab("") + ylab("") + ylim(c(-1.5,3)) + 
   geom_line(data = patient,aes(I(diffdage/365), loglymf), colour = "yellow", alpha = 3/3)
-ggplot(aes(I(diffdage/365), loglymf, group = ProjectID)) +
-  geom_line(alpha = 1/5, colour = "black") + xlab("") + ylab("") + ylim(c(-1.5,3)) + 
+
+png('artis-black.png',width = 1200, height = 800,pointsize = 30)
+lymf %>% ggplot(aes(I(diffdage/365), loglymf, group = ProjectID)) +
+  geom_line(alpha = 1/5, colour = "grey") + xlab("") + ylab("") + ylim(c(-1.5,3)) + 
   geom_line(data = patient,aes(I(diffdage/365), loglymf), colour = "gold", alpha = 1) +
   theme(plot.background = element_rect("black"),
         panel.background = element_rect("black"),
